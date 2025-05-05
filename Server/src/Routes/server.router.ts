373cfb613b2b1a4447bac15548970d8633a7ec62
Agrim2112 <agrimgupta2112@gmail.com>
2025-05-06 01:46:12 +0530

    Added firebase notifications and device token api

--- Server/src/Routes/server.router.ts
+++ Server/src/Routes/server.router.ts
@@ -9,6 +9,7 @@ import { WebSocketService } from "../Services/websocket.service";
 import { Alert } from "../Models/alert";
 import { UserLog } from "../Models/userLog";
 import { ObjectId } from "mongodb";
+import { DeviceToken } from "../Models/deviceToken";
 
 
 export const router = express.Router();
@@ -18,7 +19,7 @@ console.log("Database: ", DB.db?.databaseName);
 router.use(express.text());
 router.use(express.json());
 
-const wss = new WebSocketService(3010);
+const wss = new WebSocketService(3011);
 
 // GET
 router.get("/getallrawdata", async (_req: Request, res: Response) => {
@@ -86,7 +87,7 @@ router.post("/userLog", async (req: Request, res: Response) => {
         if (!DB.db) throw new Error("Database not connected");
 
         const { sensorId, logMessage, userId, password } = req.body;
-        
+
         if (sensorId === undefined || sensorId === null) throw new Error("No sensorId provided");
         if (!logMessage) throw new Error("No log message provided");
 
@@ -100,39 +101,78 @@ router.post("/userLog", async (req: Request, res: Response) => {
 });
 
 
-router.get("/userLog", async (req: Request, res: Response) : Promise<any> => {
+router.get("/userLog", async (req: Request, res: Response): Promise<any> => {
     try {
-      if (!DB.db) throw new Error("Database not connected");
-  
-      const userId = req.query.userId as string;
-      const password = req.query.password as string;
-  
-      if (!userId || !password) {
-        return res.status(400).send("Invalid or missing credentials ");
-      }
-  
-      const logs = await DB.db.collection<UserLog>("UserLogs").find({ userId, password }).sort({ timestamp: -1 }).toArray();
-  
-      res.status(200).send(logs);
-    } catch (error: any) {
-      res.status(500).send(error.message);
-    }
-  });
+        if (!DB.db) throw new Error("Database not connected");
 
-router.delete("/userLog/:id", async (req: Request, res: Response) : Promise<any> => {
-    try {
-      if (!DB.db) throw new Error("Database not connected");
-  
-      const { id } = req.params;
-      const result = await DB.db.collection<UserLog>("UserLogs").deleteOne({ _id: new ObjectId(id) });
-  
-      if (result.deletedCount === 0) {
-        return res.status(404).send("User log not found");
-      }
-      return res.status(200).send("User log deleted");
+        const userId = req.query.userId as string;
+        const password = req.query.password as string;
+
+        if (!userId || !password) {
+            return res.status(400).send("Invalid or missing credentials ");
+        }
+
+        const logs = await DB.db.collection<UserLog>("UserLogs").find({ userId, password }).sort({ timestamp: -1 }).toArray();
+
+        res.status(200).send(logs);
     } catch (error: any) {
-      return res.status(500).send(error.message);
+        res.status(500).send(error.message);
     }
-  });
+});
+
+router.delete("/userLog/:id", async (req: Request, res: Response): Promise<any> => {
+    try {
+        if (!DB.db) throw new Error("Database not connected");
+
+        const { id } = req.params;
+        const result = await DB.db.collection<UserLog>("UserLogs").deleteOne({ _id: new ObjectId(id) });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).send("User log not found");
+        }
+        return res.status(200).send("User log deleted");
+    } catch (error: any) {
+        return res.status(500).send(error.message);
+    }
+});
+
+router.post("/deviceToken", async (req: Request, res: Response): Promise<any> => {
+    try {
+        if (!DB.db) throw new Error("Database not connected");
+
+        const { deviceId, fcmToken } = req.body;
+        if (!deviceId || !fcmToken) {
+            return res.status(400).send("Missing deviceId or fcmToken");
+        }
+
+        const result = await DB.db.collection<DeviceToken>("DeviceTokens").findOneAndUpdate(
+            { deviceId },
+            { $set: { fcmToken, createdAt: Date.now() } },
+            { returnDocument: "after", upsert: true } // upsert inserts if not found
+        );
+
+        if (!result) {
+            return res.status(500).send("Upsert failed, no document returned.");
+        }
+
+        return res.status(200).send({
+            message: "Token saved"
+        });
+    } catch (error: any) {
+        return res.status(500).send(error.message);
+    }
+});
+
+router.get("/notifications", async (_req: Request, res: Response): Promise<any> => {
+    try {
+        if (!DB.db) throw new Error("Database not connected");
+
+        const notifications = await DB.db.collection("Notifications").find({}).sort({ timestamp: -1 }).toArray();
+
+        res.status(200).send(notifications);
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
+});
 
 // DELETE
